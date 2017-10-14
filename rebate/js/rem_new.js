37515d9a292b513e2c1e287baedefe8bf92c6d4a
郭立航 <Guolihang@users.noreply.github.com>
2017-10-15 04:04:17 +0800

    修改

--- rebate/js/rem_new.js
+++ rebate/js/rem_new.js
@@ -1,32 +1,33 @@
 (function (doc, win) {
-        var docEl = doc.documentElement,
-            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-            recalc = function () {
-                var clientWidth = docEl.clientWidth;
-                if (!clientWidth) return;
-                if(clientWidth>=800){
-                  // 这里的750 取决于设计稿的宽度
-                    docEl.style.fontSize = '25px';
-                }else if(clientWidth<800 && clientWidth>=750){
-                    docEl.style.fontSize = '23.5px';
-                    // docEl.style.fontSize = 10 * (clientWidth / 750) + 'px';
-                }else if(clientWidth<749 && clientWidth>=720){
-                    docEl.style.fontSize = '22.5px';
-                }else if(clientWidth<719 && clientWidth>=640){
-                    docEl.style.fontSize = '20px';
-                }else if(clientWidth<639 && clientWidth>=415){
-                    docEl.style.fontSize = '15px';
-                }else if(clientWidth<414 && clientWidth>=376){
-                    docEl.style.fontSize = '12px';
-                }else if(clientWidth<375 && clientWidth>=320){
-                    docEl.style.fontSize = '11px';
-                }
-            };
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if(clientWidth>=800){
+                // 这里的750 取决于设计稿的宽度
+                docEl.style.fontSize = '25px';
+            }else if(clientWidth<800 && clientWidth>=750){
+                docEl.style.fontSize = '23.5px';
+                // docEl.style.fontSize = 10 * (clientWidth / 750) + 'px';
+            }else if(clientWidth<=749 && clientWidth>=720){
+                docEl.style.fontSize = '22.5px';
+            }else if(clientWidth<=719 && clientWidth>=640){
+                docEl.style.fontSize = '20px';
+            }else if(clientWidth<=639 && clientWidth>=415){
+                docEl.style.fontSize = '15px';
+            }else if(clientWidth<=414 && clientWidth>=376){
+                docEl.style.fontSize = '12px';
+            }else if(clientWidth<=375 && clientWidth>320){
+                docEl.style.fontSize = '11px';
+            }else{
+                docEl.style.fontSize = '10px';
+            }
+        };
 
-        if (!doc.addEventListener) return;
-        win.addEventListener(resizeEvt, recalc, false);
-        doc.addEventListener('DOMContentLoaded', recalc, false);
-    })(document, window);
-
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
 
 
